58c637f9011595665f1c4f54fd88df4d920b8969
Andy Teucher <andy.teucher@gmail.com>
2013-12-02 15:05:44 -0800

    add simple param, error checking

--- R/notable.R
+++ R/notable.R
@@ -28,13 +28,15 @@
 #'    be included? (defaults to FALSE)
 #' @param hotspot Should results be limited to sightings at birding hotspots? 
 #'    (defaults to FALSE).
+#' @param simple Logical. Whether to return a simple (TRUE, default) or detailed
+#'    (FALSE) set of results fields.
 #' @param sleep Time (in seconds) before function sends API call (defaults to
 #'    zero.  Set to higher number if you are using this function in a loop with 
 #'    many API calls).
 #' @param ... additional parameters to be passed to curl.
 #' @note \code{ebirdnotable} requires that either latitude/longitude, location ID,
-#' or region be passed to the function. Multiple entries (or lack of one) will result
-#' in error.
+#' or region be passed to the function. Multiple entries will result in the most
+#' specific being used. If none is supplied, defaults to lat/lng based on your IP.
 #' @return A data.frame containing the collected information:
 #' @return "comName": species common name
 #' @return "howMany": number of individuals observed, NA if only presence was noted
@@ -50,15 +52,25 @@
 #' @return "obsValid": TRUE if observation has been deemed valid by either the 
 #'    automatic filters or a regional viewer, FALSE otherwise
 #' @return "sciName" species' scientific name
+#' @return "subnational2Code": county code (returned if simple=FALSE)
+#' @return "subnational2Name": county name (returned if simple=FALSE)
+#' @return "subnational1Code": state/province ISO code (returned if simple=FALSE)
+#' @return "subnational1Name": state/province name (returned if simple=FALSE)
+#' @return "countryCode": country ISO code (returned if simple=FALSE)
+#' @return "countryName": country name (returned if simple=FALSE)
+#' @return "firstName": observer's first name (returned if simple=FALSE)
+#' @return "lastName": observer's last name (returned if simple=FALSE)
+#' @return "subID": submission ID (returned if simple=FALSE)
+#' @return "obsID": observation ID (returned if simple=FALSE)
+#' @return "checklistID": checklist ID (returned if simple=FALSE)
+#' @return "presenceNoted": 'true' if user marked presence but did not count the
+#'   number of birds. 'false' otherwise (returned if simple=FALSE)
 #' @export
 #' @examples \dontrun{
 #' ebirdnotable(lat=42,lng=-70)
 #' ebirdnotable(locID = c('L99381','L99382'))
 #' ebirdnotable(region='US', max=10)
 #' ebirdnotable(region='US-OH', regtype='subnational1')
-#' }
-#' @examples \donttest{
-#' ebirdnotable(locID='L99381',region='US') #ERROR 
 #' }
 #' @author Rafael Maia \email{rm72@@zips.uakron.edu}
 #' @references \url{http://ebird.org/}
@@ -67,7 +79,7 @@
 ebirdnotable <-  function(lat = NULL, lng = NULL, dist = NULL, locID = NULL, 
                           region = NULL, regtype = NULL, back = NULL, 
                           max = NULL, locale = NULL, provisional = FALSE, 
-                          hotspot = FALSE, sleep = 0, 
+                          hotspot = FALSE, simple=TRUE, sleep = 0, 
                           ... #additional parameters inside curl
 ) {
   
@@ -76,6 +88,10 @@ ebirdnotable <-  function(lat = NULL, lng = NULL, dist = NULL, locID = NULL,
   Sys.sleep(sleep)
   
   if (!is.null(back)) {
+    if (back > 30) {
+      back <- 30
+      warning("'Back' supplied was > 30 days, using 30 days.")
+    }
     back <- round(back)
   }
   
@@ -92,52 +108,70 @@ ebirdnotable <-  function(lat = NULL, lng = NULL, dist = NULL, locID = NULL,
     args$hotspot <- 'true'
   }
   
-  multilocs <- length(c(lat,locID[1],region)) > 1
-  
-  if (multilocs) {
-    warning("You supplied more than one location type, using the most specific")
+  if (!simple) {
+    args$detail <- 'full'
   }
   
-  # Choose the most specific of the locations provided (lat/lng>locID>region)
+  multilocs <- length(c(lat,locID[1],region)) > 1
+  
+  # Choose most specific of the locations provided (lat/lng > locID > region)
   if (!is.null(lat)) {
-    url <- 'http://ebird.org/ws1.1/data/notable/geo/recent'
-    args$lat <- lat
-    args$lng <- lng
+    loctype <- "lat/lng"
+    if (abs(lat) > 90) {
+      stop("Please provide a latitude between -90 and 90 degrees.")
+    }
+    if (abs(lng) > 180) {
+      stop("Please provide a longitude between -180 and 180 degrees.")
+    }
   } else if (!is.null(locID)) {
+    loctype <- "locID"
     if (length(locID) > 10) {
       locID <- locID[1:10]
       warning("You supplied > 10 locations, using the first 10")
     }
     url <- 'http://ebird.org/ws1.1/data/notable/loc/recent'
     args$r <- locID
-    args$hotspot <- NULL
   } else if (!is.null(region)) {
+    loctype <- "region"
     url <- 'http://ebird.org/ws1.1/data/notable/region/recent'
-    regtype <- match.arg(regtype)
+    # regtype <- match.arg(regtype)
     args$r <- region
     args$rtype <- regtype
   } else {
     # Get IP location information from http://freegeoip.net
-    url <- 'http://ebird.org/ws1.1/data/notable/geo/recent'
+    loctype <- "lat/lng"
     loc <- fromJSON(readLines("http://freegeoip.net/json/", warn=FALSE))
-    args$lat <- loc$latitude
-    args$lng <- loc$longitude
+    lat <- loc$latitude
+    lng <- loc$longitude
     warning(paste("As no location was provided, your location", 
                   "was determined using your computer's public-facing IP", 
                   "address. This will likely not reflect your physical", 
                   "location if you are using a remote server or proxy."))
   }
   
-  if (!is.null(dist)) {
-    args$dist <- round(dist)
+  if (loctype == "lat/lng") {
+    url <- 'http://ebird.org/ws1.1/data/notable/geo/recent'
+    args$lat <- lat
+    args$lng <- lng
+    if (!is.null(dist)) {
+      if (dist > 50) {
+        dist <- 50
+        warning("Distance supplied was > 50km, using 50km.")
+      }
+      args$dist <- round(dist)
+    }
+  }
+  
+  if (multilocs) {
+    warning(paste0("You supplied more than one location type, using the most", 
+                   "specific (", loctype, ")"))
   }
   
   args <- compact(args)
   
   content <- getForm(url, 
                      .params = args, 
-                     ... ,
-                     curl = curl)
+                     ... , curl = curl)
   
   res <- fromJSON(content)
   ret <- rbind.fill(lapply(res, data.frame, stringsAsFactors=FALSE))
