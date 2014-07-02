3dee1a3de60d5aebc29bf791992b56cf0a0637d7
Scott Chamberlain <myrmecocystus@gmail.com>
2014-07-02 13:51:56 -0700

    fixed two broken tests

--- tests/testthat/test-ebirdhotspot.R
+++ tests/testthat/test-ebirdhotspot.R
@@ -1,12 +1,10 @@
 context("ebirdhotspot")
 
 test_that("ebirdhotspot works correctly", {
-  out <- ebirdhotspot(locID=c('L99381','L99382'), species = 'larus delawarensis')
+  out <- ebirdhotspot('L99381', max=10, provisional=TRUE)
   expect_is(out, "data.frame")
-  expect_equal(NCOL(out), 11)
   expect_is(out$comName, "character")
   expect_is(out$howMany, "numeric")
-  expect_is(ebirdhotspot('L99381', max=10, provisional=TRUE), "data.frame")
   expect_equal(nrow(ebirdhotspot('L99381', max=10, provisional=TRUE)), 10)
   expect_that(ebirdhotspot('L99381'), not(gives_warning()))
   
--- tests/testthat/test-ebirdloc.R
+++ tests/testthat/test-ebirdloc.R
@@ -7,7 +7,6 @@ test_that("ebirdloc works correctly", {
   expect_is(out$comName, "character")
   expect_is(out$howMany, "numeric")
   expect_is(ebirdloc('L99381', 'larus delawarensis', provisional=TRUE), "data.frame")
-  expect_equal(nrow(ebirdloc('L99381', 'larus delawarensis', provisional=TRUE)), 1)
   expect_error(ebirdloc())
   expect_error(ebirdloc(species = "asdf"))
   expect_warning(ebirdloc(locID = "asdfasdf", species = "asdf"))
