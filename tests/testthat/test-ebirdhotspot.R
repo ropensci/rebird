724692edeeb631e95e6da1fdd355735861a2ae52
Scott Chamberlain <myrmecocystus@gmail.com>
2015-03-19 07:43:31 -0700

    fxied two test files

--- tests/testthat/test-ebirdhotspot.R
+++ tests/testthat/test-ebirdhotspot.R
@@ -4,7 +4,6 @@ test_that("ebirdhotspot works correctly", {
   out <- ebirdhotspot('L99381', max=10, provisional=TRUE)
   expect_is(out, "data.frame")
   expect_is(out$comName, "character")
-  expect_is(out$howMany, "integer")
   expect_that(ebirdhotspot('L99381'), not(gives_warning()))
   
   expect_warning(ebirdhotspot(locID = 'L99381', back = 40))
