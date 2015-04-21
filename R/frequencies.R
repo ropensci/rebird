42262e599475a8c23af404ef18833025837c4390
Sebastian Pardo <sebpardo@gmail.com>
2015-04-21 01:20:14 -0700

    Add hotspot functionality & shorten month names

--- R/frequencies.R
+++ R/frequencies.R
@@ -12,17 +12,25 @@
 #'
 #' @examples
 get_freq <- function(loctype, loc, startyear, endyear, startmonth, endmonth) {
-  args <- list(cmd = "getChart", displayType = "download", getLocations = loctype, 
+  if (loctype == "county") {
+    args <- list(cmd = "getChart", displayType = "download", getLocations = loctype, 
                counties = loc, bYear = startyear, eYear = endyear, 
                bMonth = startmonth, eMonth = endmonth)
+  }
+  if (loctype == "hotspots") {
+    args <- list(cmd = "getChart", displayType = "download", getLocations = loctype, 
+                 hotspots = loc, bYear = startyear, eYear = endyear, 
+                 bMonth = startmonth, eMonth = endmonth)
+  }
+  else stop("Not a valid location type")
   
-  url <- "http://ebird.org/ebird/canada/BarChart"
+  url <- "http://ebird.org/ebird/BarChart"
   ret <- GET(url, query = args)
   stop_for_status(ret)
   asChar <- readBin(ret$content, "character")
   freq <- read.delim(text = asChar, skip = 12, 
                      stringsAsFactors = FALSE)[,-50]
-  names(freq) <- c("Species", sapply(month.name, paste ,1:4, sep="-"))
+  names(freq) <- c("Species", sapply(month.abb, paste ,1:4, sep="-"))
   freq_long <- tidyr::gather(freq[-1,], "mo_qt", "Freq", 2:length(freq), 
                              convert = TRUE)
   ss <- tidyr::gather(freq[1,-1], key = "mo_qt", value = "sample_size", 
