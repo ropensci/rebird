e2146cab7d072fffb6d8ea9d4a3870d0025038d8
ateucher <Andy.Teucher@gmail.com>
2015-04-24 07:09:58 -0700

    Add some error checking

--- R/frequencies.R
+++ R/frequencies.R
@@ -86,19 +86,19 @@ get_freq <- function(loctype, loc, startyear = 1900,
 }
 
 eloc_check <- function(loctype, loc) {
+  
   url <- "http://ebird.org/ws1.1/ref/location/list"
   
   if (loctype == "country") { 
     args <- list(rtype = "country")
-  } else
-    if (loctype == "states") {
-      args <- list(rtype = "subnational1",
-                   countryCode = substr(loc, 1, 2)) 
-    } else
-      if (loctype == "counties") {
-        args <- list(rtype = "subnational2",
-                     subnational1Code = substr(loc, 1, 5)) 
-      }
+  } else if (loctype == "states") {
+    args <- list(rtype = "subnational1", countryCode = substr(loc, 1, 2)) 
+  } else if (loctype == "counties") {
+    args <- list(rtype = "subnational2",
+                 subnational1Code = substr(loc, 1, 5)) 
+  } else {
+    stop("loctype must be one of 'country', 'states', 'counties'")
+  }
   r <- GET(url, query = args)
   loc %in% content(r)[, paste0(toupper(args$rtype), "_CODE")]
 }
