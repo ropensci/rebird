8c43edec5a4309fe128aef9adeb8c36873fa9057
Sebastian Pardo <sebpardo@gmail.com>
2015-04-25 20:00:06 -0700

    Add more ebirdloccheck tests, source fxn from environment

--- tests/testthat/test-ebirdfreq.R
+++ tests/testthat/test-ebirdfreq.R
@@ -1,12 +1,21 @@
-context("ebirdfreq")
+context("ebirdloccheck")
 
 test_that("ebirdloccheck works correctly", {
-  expect_is(rebird:::ebirdloccheck("country", "CA"), "logical")
-  expect_equal(rebird:::ebirdloccheck("country", "CA"), TRUE)
-  expect_equal(rebird:::ebirdloccheck("states", "CA-BC"), TRUE)
-  expect_equal(rebird:::ebirdloccheck("counties", "CA-BC-CP"), TRUE)
-  expect_equal(rebird:::ebirdloccheck("country", "ZZ"), FALSE)
-  expect_equal(rebird:::ebirdloccheck("states", "CA-ZZ"), FALSE)
-  expect_equal(rebird:::ebirdloccheck("counties", "CA-BC-ZZ"), FALSE)
-  expect_error(rebird:::ebirdloccheck("foo", "CA-BC-ZZ"))
+  expect_is(ebirdloccheck("country", "CA"), "logical")
+  expect_equal(ebirdloccheck("country", "CA"), TRUE)
+  expect_equal(ebirdloccheck("states", "CA-BC"), TRUE)
+  expect_equal(ebirdloccheck("counties", "CA-BC-CP"), TRUE)
+  expect_equal(ebirdloccheck("country", "ZZ"), FALSE)
+  expect_equal(ebirdloccheck("country", 1L), FALSE)
+  expect_equal(ebirdloccheck("states", "CA-ZZ"), FALSE)
+  expect_equal(ebirdloccheck("counties", "CA-BC-ZZ"), FALSE)
+  expect_error(ebirdloccheck("country", "CA", "US"))
 })
+
+test_that("ebirdloccheck fails correctly", {
+  expect_error(ebirdloccheck())
+  expect_error(ebirdloccheck("country"))
+  expect_error(ebirdloccheck("foo", "CA-BC-ZZ"))
+  expect_error(ebirdloccheck("country", "CA", "US"))
+})
+
