5d4de0c7cfa39f7f8420b41b898f5318c1acf270
Scott Chamberlain <myrmecocystus@gmail.com>
2015-05-21 10:48:57 -0700

    fixed failing ebirdfreq tests for testing failing well

--- tests/testthat/test-ebirdfreq.R
+++ tests/testthat/test-ebirdfreq.R
@@ -21,7 +21,7 @@ test_that("ebirdfreq fails correctly", {
   expect_error(ebirdfreq("hotspots", "foo123"), "Invalid hotspot code")
   expect_error(ebirdfreq("country"), "Not a valid location type")
   expect_error(ebirdfreq("counties", "CA-BC-ZZ"), "Specified location doesn't exist")
-  expect_error(ebirdfreq("hotspots", "L196159", 1901, 1902, long=FALSE))
-    expect_error(ebirdfreq("foo", "CA-BC-ZZ"))
+  expect_warning(ebirdfreq("hotspots", "L196159", 1901, 1902, long = FALSE), "No observations returned")
+  expect_error(ebirdfreq("foo", "CA-BC-ZZ"), "Not a valid location type")
 })
 
