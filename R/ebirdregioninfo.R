5da85309024e776fd96d2033b32d50451b8fe637
Sebastian Pardo <sebpardo@gmail.com>
2018-09-24 13:42:08 -0300

    Update ebirdregioninfo and test after renaming columns and removing pipes

--- R/ebirdregioninfo.R
+++ R/ebirdregioninfo.R
@@ -2,12 +2,17 @@
 #'
 #@param loctype One of: 'country', 'states', 'counties'
 #' @param loc The location code to be checked. A single location only.
-#' @param format [nameonly|namequal|detailed|detailednoqual|revdetailed|full] Different options for displaying hieararchy of the region's name. Defaults to full.
+#' @param format Different options for displaying hieararchy of the region's name: [nameonly|namequal|detailed|detailednoqual|revdetailed|full], defaults to full.
+#' @param key ebird API key. You can obtain one from https://ebird.org/api/keygen.
+#'    We strongly recommend storing it in your \code{.Renviron} file as an 
+#'    enivronment variable called \code{EBIRD_KEY}.
 #' @param ... Curl options passed on to \code{\link[httr]{GET}} 
 #'
-#' @return A tibble containing:
+#' @return A data frame containing:
 #' @return "region": name of the region, varies depending on value of "format" provided
-#' @return "bounds.minX", "bounds.maxX", "bounds.minY", "bounds.minY": lat/long bounds of the region 
+#' @return "minX", "maxX", "minY", "minY": lat/long bounds of the region 
+#' 
+#' @importFrom utils URLencode
 #' @export
 #' 
 #' @examples \dontrun{
@@ -27,20 +32,17 @@ ebirdregioninfo <- function(loc, format = "full", key = NULL, ...) {
   
   url <- paste0(ebase(), "ref/region/info/", loc)
   
-  #ebird_GET(url, args, key = key, ...)
-#  browser()
   tt <- GET(URLencode(url), 
             query = ebird_compact(args), 
             add_headers("X-eBirdApiToken" = get_key(key)), ...)
   stop_for_status(tt)
+  
   ss <- content(tt, as = "text", encoding = "UTF-8")
   json <- jsonlite::fromJSON(ss, FALSE)
   if (length(json) == 0) stop(paste("No region with code", loc))
-  as_data_frame(t(rapply(json, unlist))) %>% 
-    rename(region = result) %>% 
-    mutate_at(vars(bounds.minX:bounds.maxY), as.numeric)
-  #r <- GET(paste0(ebase(), "ref/region/info/", loc), query = args, key = key, ...)
-  #stop_for_status(r)
-  #tmp <- read.delim(text = content(r, "text", encoding = "UTF-8"), sep = ",", stringsAsFactors = FALSE)
-  #loc %in% tmp[, paste0(toupper(args$rtype), "_CODE")]
+  
+  df <- as_data_frame(t(rapply(json, unlist))) 
+  names(df) <- c("region", "minX", "maxX", "minY", "maxY")
+  df[,2:5] <- sapply(df[,2:5], as.numeric)
+  df
 }
