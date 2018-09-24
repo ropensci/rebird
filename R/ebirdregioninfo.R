b057b479d421087004f1bae65772ba2c0a6802d5
Sebastian Pardo <sebpardo@gmail.com>
2018-09-24 13:17:20 -0300

    Make ebirdregioninfo work with API 2.0 and update tests

--- R/ebirdregioninfo.R
+++ R/ebirdregioninfo.R
@@ -1,41 +1,46 @@
-#' Check if a region type is valid
+#' Region info
 #'
-#' @param loctype One of: 'country', 'states', 'counties'
-#' @param loc The location code to be checked. A single location only,
-#'   unless \code{loctype = 'country'}.
+#@param loctype One of: 'country', 'states', 'counties'
+#' @param loc The location code to be checked. A single location only.
+#' @param format [nameonly|namequal|detailed|detailednoqual|revdetailed|full] Different options for displaying hieararchy of the region's name. Defaults to full.
 #' @param ... Curl options passed on to \code{\link[httr]{GET}} 
 #'
-#' @return A logical vector of the same length as \code{loc}.
+#' @return A tibble containing:
+#' @return "region": name of the region, varies depending on value of "format" provided
+#' @return "bounds.minX", "bounds.maxX", "bounds.minY", "bounds.minY": lat/long bounds of the region 
 #' @export
 #' 
 #' @examples \dontrun{
-#' ebirdregioncheck("country", c("US", "CA"))
-#' ebirdregioncheck("states", "CA-BC")
-#' ebirdregioncheck("counties","CA-BC-GV")
+#' ebirdregioninfo("US")
+#' ebirdregioninfo("CA-BC-GV")
 #' }
 #' @author Sebastian Pardo \email{sebpardo@@gmail.com},
 #'    Andy Teucher \email{andy.teucher@@gmail.com}
 #' @references \url{http://ebird.org/}
 
-ebirdregioncheck <- function(loctype, loc, ...) {
-
-  if (loctype != "country" && length(loc) > 1) {
+ebirdregioninfo <- function(loc, format = "full", key = NULL, ...) {
+  if (length(loc) > 1) {
     stop("More than one location specified")
   }
   
-  if (loctype == "country") { 
-    args <- list(rtype = "country")
-  } else if (loctype == "states") {
-    args <- list(rtype = "subnational1", countryCode = substr(loc, 1, 2)) 
-  } else if (loctype == "counties") {
-    args <- list(rtype = "subnational2",
-                 subnational1Code = substr(loc, 1, 5)) 
-  } else {
-    stop("loctype must be one of 'country', 'states', 'counties'", call. = FALSE)
-  }
+  args <- list(regionNameFormat = format) 
   
-  r <- GET(paste0(ebase(), "ref/location/list"), query = args, ...)
-  stop_for_status(r)
-  tmp <- read.delim(text = content(r, "text", encoding = "UTF-8"), sep = ",", stringsAsFactors = FALSE)
-  loc %in% tmp[, paste0(toupper(args$rtype), "_CODE")]
+  url <- paste0(ebase(), "ref/region/info/", loc)
+  
+  #ebird_GET(url, args, key = key, ...)
+#  browser()
+  tt <- GET(URLencode(url), 
+            query = ebird_compact(args), 
+            add_headers("X-eBirdApiToken" = get_key(key)), ...)
+  stop_for_status(tt)
+  ss <- content(tt, as = "text", encoding = "UTF-8")
+  json <- jsonlite::fromJSON(ss, FALSE)
+  if (length(json) == 0) stop(paste("No region with code", loc))
+  as_data_frame(t(rapply(json, unlist))) %>% 
+    rename(region = result) %>% 
+    mutate_at(vars(bounds.minX:bounds.maxY), as.numeric)
+  #r <- GET(paste0(ebase(), "ref/region/info/", loc), query = args, key = key, ...)
+  #stop_for_status(r)
+  #tmp <- read.delim(text = content(r, "text", encoding = "UTF-8"), sep = ",", stringsAsFactors = FALSE)
+  #loc %in% tmp[, paste0(toupper(args$rtype), "_CODE")]
 }
--- tests/testthat/test-ebirdregioninfo.R
+++ tests/testthat/test-ebirdregioninfo.R
@@ -1,25 +1,23 @@
-context("ebirdregioncheck")
+context("ebirdregioninfo")
 
-test_that("ebirdregioncheck works correctly", {
+test_that("ebirdregioninfo works correctly", {
   skip_on_cran()
   
-  expect_is(ebirdregioncheck("country", "CA"), "logical")
-  expect_equal(ebirdregioncheck("country", "CA"), TRUE)
-  expect_equal(ebirdregioncheck("states", "CA-BC"), TRUE)
-  expect_equal(ebirdregioncheck("counties", "CA-BC-CP"), TRUE)
-  expect_equal(ebirdregioncheck("country", "ZZ"), FALSE)
-  expect_equal(ebirdregioncheck("country", 1L), FALSE)
-  expect_equal(ebirdregioncheck("states", "CA-ZZ"), FALSE)
-  expect_equal(ebirdregioncheck("counties", "CA-BC-ZZ"), FALSE)
-  expect_error(ebirdregioncheck("country", "CA", "US"))
+  us <- ebirdregioninfo("US")
+  
+  expect_is(us, "data.frame")
+  expect_equal(us$region, "United States")
+  expect_equal(NROW(us), 1)
+  expect_equal(NCOL(us), 5)
+  expect_is(us$region, "character")
+  expect_is(us$bounds.minX, "numeric")
 })
 
-test_that("ebirdregioncheck fails correctly", {
+test_that("ebirdregioninfo fails correctly", {
   skip_on_cran()
   
-  expect_error(ebirdregioncheck())
-  expect_error(ebirdregioncheck("country"))
-  expect_error(ebirdregioncheck("foo", "CA-BC-ZZ"))
-  expect_error(ebirdregioncheck("country", "CA", "US"))
+  expect_error(ebirdregioninfo())
+  expect_error(ebirdregioninfo("foo"), "Internal Server Error \\(HTTP 500\\).")
+  expect_error(ebirdregioninfo("CA-BC-ZZ"), "No region with code")
 })
 
