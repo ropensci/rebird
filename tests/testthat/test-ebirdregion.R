3f80814137b4d91cabdb60f3a9903a135e5fdf49
Sebastian Pardo <sebpardo@gmail.com>
2018-09-25 16:13:31 -0300

    Change variable name from region to loc in tests

--- tests/testthat/test-ebirdregion.R
+++ tests/testthat/test-ebirdregion.R
@@ -3,7 +3,7 @@ context("ebirdregion")
 test_that("ebirdregion works correctly", {
   skip_on_cran()
   
-  out <- ebirdregion(region = 'US', species = 'btbwar', max = 50)
+  out <- ebirdregion(loc = 'US', species = 'btbwar', max = 50)
   expect_is(out, "data.frame")
   expect_equal(ncol(out), 12)
   expect_is(out$comName, "character")
@@ -11,8 +11,8 @@ test_that("ebirdregion works correctly", {
 
   expect_equal(dim(ebirdregion('US-OH', max=10, provisional=TRUE, hotspot=TRUE)), c(10,12))
   
-  res <- ebirdregion(region = 'US-CA', max = 10)
+  res <- ebirdregion(loc = 'US-CA', max = 10)
   expect_equal(ncol(res), 12)
   
-  expect_equal(ncol(ebirdregion(region = 'US', species = 'coohaw')), 12)
+  expect_equal(ncol(ebirdregion(loc = 'US', species = 'coohaw')), 12)
 })
