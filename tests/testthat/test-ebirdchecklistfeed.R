901fc4da12a56482d30e57286f78041ffe9a1595
Sebastian Pardo <sebpardo@gmail.com>
2020-04-09 10:15:51 -0300

    Add more checklistfeed tests

--- tests/testthat/test-ebirdchecklistfeed.R
+++ tests/testthat/test-ebirdchecklistfeed.R
@@ -3,16 +3,18 @@ context("ebirdchecklistfeed")
 test_that("ebirdchecklistfeed succeeds reproducibly", {
   skip_on_cran()
   
-  out1 <- ebirdchecklistfeed(loc = "L207391", date = "2020-03-24", max = 10)
+  out1 <- ebirdchecklistfeed(loc = "L207391", date = "2020-03-24", max = 3)
   expect_is(out1, "data.frame")
-  expect_equal(nrow(out1), 8)
-  expect_equal(ncol(out1), 5)
+  expect_is(out1$userDisplayName, "character")
+  expect_equal(nrow(out1), 3)
+  expect_equal(ncol(out1), 8)
 })
 
 test_that("ebirdchecklistfeed errors for bad input", {
   skip_on_cran()
   
   expect_error(ebirdchecklistfeed(loc = "L207391", date = "2020-03-25", max = 201))
-  expect_error(ebirdchecklistfeed(loc = "L207391", date = "2021-03-25"))
+  expect_error(ebirdchecklistfeed(loc = "L207391", date = "2121-03-25"))
   expect_error(ebirdchecklistfeed(loc = "L207391", date = "03-25-2020"))
+  expect_error(ebirdchecklistfeed(loc = "LL207391", date = "2020-03-25", max = 10))
 })
