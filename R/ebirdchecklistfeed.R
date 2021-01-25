f1d0e406be5a284ec9d6f1101f610e3f4c371e8d
Sebastian Pardo <sebpardo@gmail.com>
2021-01-25 13:21:42 -0400

    Add warning when setting max results above limit, fix typos & tests

--- R/ebirdchecklistfeed.R
+++ R/ebirdchecklistfeed.R
@@ -38,11 +38,16 @@ ebirdchecklistfeed <-  function(loc, date, max = 10, sleep = 0, key = NULL, ...)
 {
   historicDate = as.Date(date)
   if (Sys.Date() < historicDate) {
-    stop(paste0("date must be in the past"))
+    stop(paste0("Date must be in the past."))
   }
-  if (historicDate < '1800-01-01'){
-    stop(paste0("date must be on or after 1800-01-01"))
+  if (historicDate < '1800-01-01') {
+    stop(paste0("Date must be on or after 1800-01-01."))
   }
+  if (max > 200) {
+    max <- 200
+    warning("Maximum number of results supplied was > 200, using 200.")
+  }
+  
   Sys.sleep(sleep)
 
   url <- paste0(ebase(), 'product/lists/', loc, '/',format(historicDate, "%Y"),'/',
--- tests/testthat/test-ebirdchecklistfeed.R
+++ tests/testthat/test-ebirdchecklistfeed.R
@@ -13,7 +13,7 @@ test_that("ebirdchecklistfeed succeeds reproducibly", {
 test_that("ebirdchecklistfeed errors for bad input", {
   skip_on_cran()
   
-  expect_error(ebirdchecklistfeed(loc = "L207391", date = "2020-03-25", max = 201))
+  expect_warning(ebirdchecklistfeed(loc = "L207391", date = "2020-03-24", max = 250))
   expect_error(ebirdchecklistfeed(loc = "L207391", date = "2121-03-25"))
   expect_error(ebirdchecklistfeed(loc = "L207391", date = "03-25-2020"))
   expect_error(ebirdchecklistfeed(loc = "LL207391", date = "2020-03-25", max = 10))
