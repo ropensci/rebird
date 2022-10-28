4601e549e42400b9d3b2e1a161d86d97ca888837
rafnuss <rafnuss@gmail.com>
2022-10-28 15:18:50 -0400

    Add `ebirdchecklist()`

---
+++ R/ebirdchecklist.R
@@ -0,0 +1,35 @@
+#' View Checklist
+#'
+#' @param subId The checklist identifier
+#' @param key eBird API key. You can obtain one from https://ebird.org/api/keygen.
+#'    We strongly recommend storing it in your \code{.Renviron} file as an
+#'    environment variable called \code{EBIRD_KEY}.
+#' @param ... Curl options passed on to \code{\link[httr]{GET}}
+#'
+#' @return A data.frame containing:
+#' @export
+#'
+#'
+#' @examples \dontrun{
+#' ebirdchecklist("S121423354")
+#' }
+#' @references \url{http://ebird.org/}
+ebirdchecklist <- function(subId, sleep = 0, key = NULL, ...) {
+
+  url <- paste0(ebase(), "product/checklist/view/", subId)
+
+  args <- list()
+
+  Sys.sleep(sleep)
+
+  tt <- GET(URLencode(url),
+            query = ebird_compact(args),
+            add_headers("X-eBirdApiToken" = get_key(key)), # removed config = add_headers(...
+            # to allow config = to be specified in ...
+            ...)
+
+  content(tt, as = "text", encoding = "UTF-8") %>%
+    fromJSON(FALSE) %>%
+    purrr::keep(!names(.) %in% c("subAux","subAuxAi")) %>%
+    as_tibble()
+}
