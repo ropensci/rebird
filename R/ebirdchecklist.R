cdae2052ad7cc0028d4c0ebbad83e1ef814d0775
Richard Littauer <richard+github@burntfen.com>
2024-02-02 10:57:27 -0500

    Change order of param documentation

--- R/ebirdchecklist.R
+++ R/ebirdchecklist.R
@@ -1,13 +1,13 @@
 #' View Checklist
 #'
 #' @param subId The checklist identifier
+#' @param sleep Time (in seconds) before function sends API call (defaults to
+#'    zero. Set to higher number if you are using this function in a loop with
+#'    many API calls).
 #' @param key eBird API key. You can obtain one from https://ebird.org/api/keygen.
 #'    We strongly recommend storing it in your \code{.Renviron} file as an
 #'    environment variable called \code{EBIRD_KEY}.
 #' @param ... Curl options passed on to \code{\link[httr]{GET}}
-#' @param sleep Time (in seconds) before function sends API call (defaults to
-#'    zero. Set to higher number if you are using this function in a loop with
-#'    many API calls).
 #'
 #' @return A data.frame containing:
 #' @export
