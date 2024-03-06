1ebe35dacf4d1a5f92484f3ea6470464fe31dd89
Dave Slager <slager@users.noreply.github.com>
2024-03-05 22:36:17 -0800

    vcr tests for ebirdhotspot

--- tests/testthat/test-ebirdhotspot.R
+++ tests/testthat/test-ebirdhotspot.R
@@ -1,13 +1,10 @@
-context("ebirdhotspot")
+vcr::use_cassette("ebirdhotspot", {
+  test_that("ebirdhotspot works correctly", {
 
-test_that("ebirdhotspot works correctly", {
-  skip_on_cran()
-  skip_on_ci()
-  
-  expect_warning(out <- ebirdhotspot('L99381', max = 10, provisional = TRUE))
-  expect_is(out, "data.frame")
-  expect_is(out$comName, "character")
-  expect_warning(ebirdhotspot('L99381'))
-  
-  expect_warning(expect_error(ebirdhotspot(locID = 'L99381', back = 40)))
+    expect_warning(out <- ebirdhotspot('L99381', max = 3, provisional = TRUE))
+    expect_is(out, "data.frame")
+    expect_is(out$comName, "character")
+    expect_warning(expect_error(ebirdhotspot(locID = letters)))
+    expect_warning(expect_error(ebirdhotspot(locID = 'L99381', back = 40)))
+  })
 })
