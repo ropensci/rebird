7396de6bfbd7ade0a7e1fc345e46902fe9a55d7d
Dave Slager <slager@users.noreply.github.com>
2024-03-07 19:32:33 -0800

    vcr tests for ebirdnotable

--- tests/testthat/test-ebirdnotable.R
+++ tests/testthat/test-ebirdnotable.R
@@ -1,23 +1,28 @@
-context("ebirdnotable")
+vcr::use_cassette("ebirdnotable", {
+  test_that("ebirdnotable works correctly", {
+    sys_tm <- system.time(
+      out <- ebirdnotable(lat=42, lng=-70, dist = 50, max = 2, sleep = 1)
+      )
+    expect_gt(sys_tm['elapsed'], 1)
+    expect_is(out, "data.frame")
+    expect_equal(dim(out), c(2,13))
+    expect_is(out$comName, "character")
+    expect_is(out$howMany, "integer")
 
-test_that("ebirdnotable works correctly", {
-  skip_on_cran()
-  skip_on_ci()
-  
-  out <- ebirdnotable(lat=42, lng=-70, max = 40)
-  expect_is(out, "data.frame")
-  expect_equal(dim(out), c(40,13))
-  expect_is(out$comName, "character")
-  expect_is(out$howMany, "integer")
+    oh <- ebirdnotable(region='US-OH', max = 2, regtype='subnational1',
+                       provisional = TRUE, hotspot = TRUE, simple = FALSE)
+    expect_gte(NCOL(oh), 28)
 
-  expect_equal(NCOL(ebirdnotable(region='US-OH', regtype='subnational1')), 14)
-  
-  simpler <- ebirdnotable(lat=42, lng=-70, max = 40, simple = TRUE)
-#   lesssimpler <- ebirdnotable(lat=42, lng=-70, max = 40, simple = FALSE)
-  expect_equal(dim(simpler), c(40,13))
-#   expect_equal(dim(lesssimpler), c(40,24))
-  
-  expect_gt(
-    system.time(ebirdnotable(lat=42, lng=-70, max = 10, sleep = 1))[[3]]
-    , 1)
+    expect_warning(
+      simpler <- ebirdnotable(lat=42, lng=-70, back = 31, max = 2, simple = TRUE),
+      "using 30 days")
+    expect_equal(dim(simpler), c(2,13))
+
+    expect_error(ebirdnotable(lat = 91, lng = 45), 'latitude')
+    expect_error(ebirdnotable(lat = 45, lng = 181), 'longitude')
+    expect_warning(
+      ebirdnotable(locID = rep('US-OH', 11), max = 2, regtype = 'subnational1'),
+      '10 locations'
+    )
+  })
 })
