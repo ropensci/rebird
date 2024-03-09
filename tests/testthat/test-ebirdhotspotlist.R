a46be024c4573b213fbc3f66a2f4e0e6f3eb62b2
Dave Slager <slager@users.noreply.github.com>
2024-03-08 23:35:51 -0800

    vcr tests for hotspotlist

--- tests/testthat/test-ebirdhotspotlist.R
+++ tests/testthat/test-ebirdhotspotlist.R
@@ -1,49 +1,37 @@
-context("ebirdhotspotlist")
-
-test_that("ebirdhotspotlist works correctly", {
-  skip_on_cran()
-  skip_on_ci()
+vcr::use_cassette("ebirdhotspotlist", {
+  test_that("ebirdhotspotlist works correctly", {
+    common_tests <- function(i){
+      expect_true(inherits(i, "data.frame"))
+      expect_true(inherits(i, "tbl_df"))
+      expect_gte(NCOL(i), 8)
+      expect_true(inherits(i$locId, "character"))
+      expect_true(inherits(i$numSpeciesAllTime, "integer"))
+    }
+    out1 <- ebirdhotspotlist("VA")
+    common_tests(out1)
+    expect_message(
+      out2 <- ebirdhotspotlist(lat = 37.698, lng = -123.008, dist = 1),
+      "No region code provided"
+    )
+    common_tests(out2)
+    expect_warning(
+      out3 <- ebirdhotspotlist("VA", back = 31),
+      "using 30 days"
+    )
+    common_tests(out3)
+    expect_message(
+      expect_warning(ebirdhotspotlist(lat = 0, lng = 0, dist = 1000))
+    )
+  })
   
-  out <- ebirdhotspotlist("CA-NS-YA")
-  out2 <- ebirdhotspotlist(lat = 30, lng = -90, dist = 10)
-  out3 <- ebirdhotspotlist(lat = 30, lng = -90, dist = 5)
-  out4 <- ebirdhotspotlist("CA-NS-YA", back = 7)
-  
-  expect_is(out, "data.frame")
-  expect_is(out, "tbl_df")
-  expect_is(out2, "data.frame")
-  expect_is(out2, "tbl_df")
-  expect_gt(NCOL(out), 8)
-  expect_gt(nrow(out2), nrow(out3))
-  expect_gt(nrow(out), nrow(out4))
-  
-  expect_is(out$locId, "character")
-  expect_is(out$numSpeciesAllTime, "integer")
-  
-  expect_warning(ebirdhotspotlist(lat = 0, lng = 0, dist = 1000))
-  expect_warning(ebirdhotspotlist(lat = 0, lng = 0, dist = 10, back = 31))
-  
-  expect_warning(
-    ebirdhotspotlist(),
-    "As a complete lat/long pair was not provided"
-  )
-})
-
-test_that("ebirdhotspotlist fails correctly", {
-  skip_on_cran()
-  
-  expect_error(ebirdhotspotlist(c("CA-NS-HL","CA-NS-YA")))
-  expect_error(ebirdhotspotlist(""))
-  expect_error(ebirdhotspotlist("foobar"))
-  expect_error(ebirdhotspotlist("CA-NS-HA"))
-  expect_error(ebirdhotspotlist(lat = 40))
-  expect_error(ebirdhotspotlist(lat = -91, lng = 0))
-  expect_error(ebirdhotspotlist(lat = 0, lng = 181))
-  expect_error(ebirdhotspotlist(lat = 51.5, lng = 0, dist = -1))
-})
-
-test_that_without_key("ebirdhotspotinfo doesn't work with dummy API key", {
-
-  expect_error(ebirdhotspotlist("CA-NS-YA")) # Doesn't work without API key
-  expect_error(ebirdhotspotlist("CA-NS-YA", key = "foobar"))
+  test_that("ebirdhotspotlist fails correctly", {
+    expect_error(ebirdhotspotlist(c("CA-NS-HL","CA-NS-YA")))
+    expect_error(ebirdhotspotlist(""))
+    expect_error(ebirdhotspotlist("foobar"))
+    expect_error(ebirdhotspotlist("CA-NS-HA"))
+    expect_error(ebirdhotspotlist(lat = 40))
+    expect_error(ebirdhotspotlist(lat = -91, lng = 0))
+    expect_error(ebirdhotspotlist(lat = 0, lng = 181))
+    expect_error(ebirdhotspotlist(lat = 51.5, lng = 0, dist = -1))
+  })
 })
