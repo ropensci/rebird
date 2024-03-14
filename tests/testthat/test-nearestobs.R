37ea3f57842bba8d20a02f9bdb14b725b0f93668
Dave Slager <slager@users.noreply.github.com>
2024-03-13 22:12:16 -0700

    vcr tests for nearestobs

--- tests/testthat/test-nearestobs.R
+++ tests/testthat/test-nearestobs.R
@@ -1,25 +1,27 @@
-context("nearestobs")
-
-test_that("nearestobs works correctly", {
-  skip_on_cran()
-  skip_on_ci()
+vcr::use_cassette("nearestobs", {
+  test_that("nearestobs works correctly", {
+    
+    out <- expect_warning(
+      nearestobs('cangoo', 42.46, -76.5, back = 30.5, max = 2),
+      "using 30 days"
+    )
+    out2 <- nearestobs('cangoo', 42.46, -76.5, max=2, provisional=TRUE, hotspot=TRUE)
+    
+    expect_true(inherits(out, "data.frame"))
+    expect_true(inherits(out2, "data.frame"))
+    
+    expect_equal(NCOL(out), 13)
+    expect_equal(NCOL(out2), 13)
+    
+    expect_true(inherits(out$comName, "character"))
+    expect_true(inherits(out$lng, "numeric"))
+  })
   
-  out <- nearestobs('cangoo', 42, -76)
-  out2 <- nearestobs('cangoo', 42,-76, max=10, provisional=TRUE, hotspot=TRUE)
-  
-  expect_is(out, "data.frame")
-  expect_is(out2, "data.frame")
-  
-  expect_equal(NCOL(out), 13)
-  expect_equal(NCOL(out2), 13)
-  
-  expect_is(out$comName, "character")
-  expect_is(out$lng, "numeric")
-})
-
-test_that("ebirdloc fails correctly", {
-  skip_on_cran()
-  
-  expect_error(suppressWarnings(nearestobs("asf")))
-  expect_error(nearestobs('asf', 42, -76))
+  test_that("ebirdloc fails correctly", {
+    
+    #expect_error(suppressWarnings(nearestobs("asf")))
+    expect_error(nearestobs('asf', 42, -76))
+    expect_error(nearestobs('cangoo', 91, -76))
+    expect_error(nearestobs('cangoo', 42, -181))
+  })
 })
