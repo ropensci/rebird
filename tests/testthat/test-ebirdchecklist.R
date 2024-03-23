1384d131c38dec03d24317b4117d237d1f3302ab
Dave Slager <slager@users.noreply.github.com>
2024-03-22 23:03:59 -0700

    vcr tests for ebirdchecklist

--- tests/testthat/test-ebirdchecklist.R
+++ tests/testthat/test-ebirdchecklist.R
@@ -1,25 +1,27 @@
-test_that("ebirdchecklist succeeds reproducibly", {
+vcr::use_cassette("ebirdchecklist", {
+  test_that("ebirdchecklist succeeds reproducibly", {
 
-  expect_no_error(out1 <- ebirdchecklist("S117450946"))
+    expect_no_error(out1 <- ebirdchecklist("S117450946"))
 
-  # check all list-columns removed during preprocessing
-  expect_false(any(vapply(out1, is.list, logical(1))))
+    # check all list-columns removed during preprocessing
+    expect_false(any(vapply(out1, is.list, logical(1))))
 
-  # Works with breeding code
-  expect_true('ON' %in% out1$auxCode)
+    # Works with breeding code
+    expect_true('ON' %in% out1$auxCode)
 
-  expect_is(out1, "data.frame")
-  expect_true(nrow(out1) == 6)
-  expect_true(ncol(out1) > 0)
-  expect_true("checklistId" %in% names(out1))
-  expect_equal(out1$checklistId[1], "CL24321")
+    expect_is(out1, "data.frame")
+    expect_true(nrow(out1) == 6)
+    expect_true(ncol(out1) > 0)
+    expect_true("checklistId" %in% names(out1))
+    expect_equal(out1$checklistId[1], "CL24321")
 
-})
+  })
 
-test_that("ebirdchecklist errors for bad input", {
+  test_that("ebirdchecklist errors for bad input", {
 
-  invalid_checklist_id <- "invalid_id"
+    invalid_checklist_id <- "invalid_id"
 
-  # Expect an error and check if the error message matches the expected pattern
-  expect_error(ebirdchecklist(invalid_checklist_id), "subId is invalid")
+    # Expect an error and check if the error message matches the expected pattern
+    expect_error(ebirdchecklist(invalid_checklist_id), "subId is invalid")
+  })
 })
