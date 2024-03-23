dd35115f35eab9dce0d00f5cb1c6b6ee9a958eca
Dave Slager <slager@users.noreply.github.com>
2024-03-22 21:50:51 -0700

    tweak, info arg is deprecated

--- tests/testthat/test-ebirdchecklist.R
+++ tests/testthat/test-ebirdchecklist.R
@@ -13,12 +13,10 @@ test_that("ebirdchecklist succeeds reproducibly", {
   expect_is(out1, "data.frame")
   expect_true(nrow(out1) > 0)
   expect_true(ncol(out1) > 0)
-  expect_true("checklistId" %in% names(out1),
-              info = "checklistId column not found in the output")
+  expect_true("checklistId" %in% names(out1))
 
   # Check if the first row's checklistId is "CL28273"
-  expect_equal(out1$checklistId[1], "CL28273",
-               info = "First row's checklistId is not CL28273")
+  expect_equal(out1$checklistId[1], "CL28273")
 })
 
 test_that("ebirdchecklist errors for bad input", {
