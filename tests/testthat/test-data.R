be130305775c476328cce65813528e0e619ef44f
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-07-12 22:26:58 -0400

    Update test vars

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,7 +2,7 @@ test_that("tax dataset version is latest version", {
   latest_version <- ebirdtaxonomyversion(latest_only = TRUE)
   temp_env <- new.env()
   data("tax", envir = temp_env)
-  dataset_tax_version <- temp_env$tax
-  dataset_tax_version_version <- attr(dataset_tax_version, "version")
-  expect_equal(latest_version, dataset_tax_version_version)
+  dataset_tax <- temp_env$tax
+  dataset_tax_version <- attr(dataset_tax, "version")
+  expect_equal(latest_version, dataset_tax_version)
 })
