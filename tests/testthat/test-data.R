14cfeb37be15534e4c5955ca4ea752802c9366f7
Jordan Bradford <36420801+jrdnbradford@users.noreply.github.com>
2024-07-28 16:09:00 -0400

    Add failed test

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -2,5 +2,5 @@ test_that("tax dataset version is latest version", {
   latest_version <- subset(ebirdtaxonomyversion(), latest == TRUE)$authorityVer
   tax <- rebird::tax
   dataset_tax_version <- attr(tax, "version")
-  expect_equal(latest_version, dataset_tax_version)
+  expect_equal(latest_version, "dataset_tax_version")
 })
